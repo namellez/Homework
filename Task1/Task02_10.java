41dd332338b80db80a64457803388bc4649fdda8
namellez <namellez@gmail.com>
2017-10-22 15:39:32 +0300

    Add files via upload

--- Task1/Task02_10.java
+++ Task1/Task02_10.java
@@ -6,6 +6,17 @@ public class Task02_10 {
 
     public static void main(String[] args) {
 
+    double x = 0;
+    double y = 0;
+
+        System.out.println("enter x: ");
+        Scanner sc = new Scanner (System.in);
+        x = sc.nextDouble();
+
+        if (x<=-3) y=9;
+        if (x>3) y= 1/(x*x +1);
+
+        System.out.println("y = " + y);
 
     }
 
